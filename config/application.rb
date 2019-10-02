d2e890db12ce51ff304f46b065e906c7dbfc7353
hanafi <hanafi@hanafis-MacBook-Pro.local>
2019-10-02 23:14:49 +0700

    acsess image asset

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Ahad
   class Application < Rails::Application
 
+    config.assets.compile = true
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
